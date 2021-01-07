f1b1b6166c51b5c7d2ffdda86c723e932f74dd2a
juandsuarezz <1486@holbertonschool.com>
2021-01-07 14:14:49 -0500

    Quality Settings

---
+++ 0x04-unity_publishing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(PlayMaze);
+        quitButton.onClick.AddListener(QuitMaze);
+    }
+
+    public void PlayMaze()
+    {
+        trapMat.color = Color.red;
+        goalMat.color = Color.green;
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        SceneManager.LoadScene("maze");
+    }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
---
+++ 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1000f;
+    private int score = 0;
+    public int health = 5;
+
+    public Text scoreText;
+    public Text healthText;
+    public Text winLoseText;
+
+    public Image winLoseBG;
+    public GameObject winLose;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playermove();
+        CheckHealth();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    void playermove()
+    {
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void CheckHealth()
+    {
+        if (health == 0)
+        {
+            //Debug.Log("Game Over!");
+            SetGameOver();
+            StartCoroutine(LoadScene(3));
+            //SceneManager.LoadScene("maze");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            Destroy(other.gameObject);
+            SetScoreText();
+            //Debug.Log($"Score: {score}");
+        }
+
+        if (other.tag == "Trap")
+        {
+            health -= 1;
+            SetHealthText();
+            //Debug.Log($"Health: {health}");
+        }
+
+        if (other.tag == "Goal")
+        {
+            SetWin();
+            StartCoroutine(LoadScene(3));
+            //Debug.Log("You win!");
+        }
+    }
+
+    void SetScoreText()
+    {
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
+    void SetWin()
+    {
+        winLose.SetActive(true);
+        winLoseBG.color = Color.green;
+        winLoseText.text = "You win!";
+        winLoseText.color = Color.black;
+    }
+
+    void SetGameOver()
+    {
+        winLose.SetActive(true);
+        winLoseBG.color = Color.red;
+        winLoseText.text = "Game Over!";
+        winLoseText.color = Color.white;
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        health = 5;
+        score = 0;
+        SceneManager.LoadScene("maze");
+    }
+}
