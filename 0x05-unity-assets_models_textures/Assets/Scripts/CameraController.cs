050242c9d99fc97e8117c8bcf4f71b58eb021f86
juandsuarezz <1486@holbertonschool.com>
2021-02-03 15:14:46 -0500

    First Update

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+///<summary>Handles camera control, allowing player to rotate the camera with the mouse</summary>
+public class CameraController : MonoBehaviour
+{
+    public float turn = 3f;
+    public Transform player;
+    private Transform tsfrm;
+    private Vector3 offset;
+    private Quaternion turnX;
+    private Quaternion turnY;
+
+    void Awake()
+    {
+        tsfrm = GetComponent<Transform>();
+    }
+
+    void Start()
+    {   
+        Cursor.visible = false;
+        offset = new Vector3(0, 1.25f, -6.25f);
+    }
+
+    void LateUpdate()
+    {
+        turnX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turn, Vector3.up);
+        turnY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turn, Vector3.left);
+        offset = turnX * turnY * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position + new Vector3(0, 0.24f, 0));
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+///<summary>Timer counts up as player traverses the level</summary>
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    private int min = 0;
+    private float sec = 0.00f;
+    private float time = 0f;
+    private string text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        min = (int)(time / 60);
+        sec = (time % 60f);
+        text = min.ToString() + ":" + sec.ToString("00.00");
+        TimerText.text = text;
+    }
+}
